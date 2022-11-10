f25681bd42135349dc258cff05f506b19afafbcf
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-11-10 11:40:42 -0500

    Q3 A2, can find orientation of turtle wrt to dest and decide on rot direction

--- a2/scripts/robot_driver.py
+++ a2/scripts/robot_driver.py
@@ -22,47 +22,59 @@ class robot_driver():
 
         rospy.init_node('robot_driver', anonymous=False)
 
-        self.pub = rospy.Publisher('/turtle1/test', Pose, queue_size=10)
-        # self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        self.pub = rospy.Publisher('/turtle1/test', Pose, queue_size=10) # TODO: delete this after test
+        # self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10) # TODO: uncomment this after test
         self.sub_dest = rospy.Subscriber('/turtle1/destination', Pose, self.dest_coord)
         self.sub_pose = rospy.Subscriber('/turtle1/pose', Pose, self.pose_coord)
+        
+        # TODO: initialise this better
+        self.HT_0_d = self.HT_builder(self.z_elemental(0), np.array([0]*3).reshape(-1,1))
         
     
     def dest_coord(self, received_msg):
         self.dest = received_msg
         
-        # # R_0_d stands for destination(d) orientation wrt world frame(0)
-        # self.R_0_d = self.z_elemental(np.arctan2(self.dest.y, self.dest.x))
+        # R_0_d stands for destination(d) orientation wrt world frame(0)
+        R_0_d = self.z_elemental(np.arctan2(self.dest.y, self.dest.x))
+        
+        # position of dest wrt to world frame
+        p_0 = np.array([self.dest.x, self.dest.y, 0]).reshape(-1,1)
+        self.HT_0_d = self.HT_builder(R_0_d, p_0)
         
     
     def pose_coord(self, received_msg):
         self.pose = received_msg
-        self.publish_test() # TODO: Remove fter test
+        self.publish_test() # TODO: Remove after test
         
         
     def publish_test(self):
-        self.vel.x = self.dest.x - self.pose.x
-        self.vel.y = self.dest.y - self.pose.y
+        self.direction_finder()
+        self.vel.angular_velocity = self.direction
+        self.vel.theta = self.angle_t_d
         self.pub.publish(self.vel)
 
+        """
+        use homogeneous matrices
+        """
+        
+    def HT_t_d_calc(self):        
+        # R_0_t stands for turtle (t) orientation wrt world frame(0)
+        R_0_t = self.z_elemental(self.pose.theta)
+        q_0 = np.array([self.pose.x, self.pose.y, 0]).reshape(-1,1)
+        self.HT_0_t = self.HT_builder(R_0_t, q_0)
+        self.HT_t_d = np.dot(np.linalg.inv(self.HT_0_t), self.HT_0_d)
+        
+        # self.R_t_d = np.dot(self.R_0_t.T, self.R_0_d)
+        
+    def rot_t_d_angle(self):
+        self.HT_t_d_calc()
+        self.angle_t_d = np.arctan2(self.HT_t_d[1,3], self.HT_t_d[0,3])
         
         
-    # def rot_t_d(self):
-    #     self.pose_coord()
-        
-    #     # R_0_t stands for turtle (t) orientation wrt world frame(0)
-    #     self.R_0_t = self.z_elemental(self.pose.theta)
-    #     self.R_t_d = self.R_0_t.T@self.R_0_d
-        
-    # def rot_t_d_angle(self):
-    #     self.rot_t_d()
-    #     self.angle_t_d = np.arctan2(self.R_t_d[1,0], self.R_t_d[0,0])
-        
-        
-    # def direction_finder(self):
-    #     self.rot_t_d_angle()
-    #     self.direction = -self.rot_spd*np.sign(self.angle_t_d) # if angle is -tiv, must turn CCW (+tiv rot)
-    #     self.rot_drvr()
+    def direction_finder(self):
+        self.rot_t_d_angle()
+        self.direction = self.rot_spd*np.sign(self.angle_t_d) # if angle is -tiv, must turn CCW (+tiv rot)
+        # self.rot_drvr() # TODO: uncomment
         
     # def rot_drvr(self):
     #     while(abs(self.angle_t_d) > robot_driver.eps):
@@ -72,12 +84,18 @@ class robot_driver():
     # def turtle_drvr(self):
     #     pass
         
-    # def z_elemental(self, gamma):            
-    #     R_z = np.array([[np.cos(gamma), -np.sin(gamma), 0],
-    #                     [np.sin(gamma), np.cos(gamma), 0],
-    #                     [0, 0, 1]])
-    #     return R_z
+    def z_elemental(self, gamma):            
+        R_z = np.array([[np.cos(gamma), -np.sin(gamma), 0],
+                        [np.sin(gamma), np.cos(gamma), 0],
+                        [0, 0, 1]])
+        
+        return R_z
     
+    
+    def HT_builder(self, R, p):
+        arr_zeros = np.array([[0, 0, 0, 1]])
+        
+        return np.vstack((np.hstack((R, p)), arr_zeros))
     
     # def remaining_dist(self):
     #     self.x_remaining = self.pose.x - self.dest.x
