a209e6561c19762a5d5ef37187c4c5db40f73bb4
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-11-10 21:29:47 -0500

    A2 Q3 working with 20Hz delay

--- a2/scripts/robot_driver.py
+++ a2/scripts/robot_driver.py
@@ -7,10 +7,13 @@ import numpy as np
 
 class robot_driver():
     
-    eps = 1e-2
+    eps = 8e-2
     dist_arrived = 10e-2
     
     def __init__(self, rot_spd, drv_spd):
+        rospy.init_node('robot_driver', anonymous=False)
+        
+        self.rate = rospy.Rate(20)
         
         self.rot_spd = rot_spd
         self.drv_spd = drv_spd
@@ -18,15 +21,14 @@ class robot_driver():
         self.dest = Pose()
         self.pose = Pose()
         self.vel = Twist()
-
-        rospy.init_node('robot_driver', anonymous=False)
-
+        
+        # self.pub = rospy.Publisher('/turtle1/test', Twist, queue_size=10)
         self.pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
         self.sub_dest = rospy.Subscriber('/turtle1/destination', Pose, self.dest_coord)
         self.sub_pose = rospy.Subscriber('/turtle1/pose', Pose, self.pose_coord)
         
         # TODO: initialise this better
-        self.HT_0_d = self.HT_builder(self.z_elemental(0), np.array([0]*3).reshape(-1,1))
+        self.HT_0_d = self.HT_builder(self.z_elemental(0), np.array([5.5, 5.5, 0]).reshape(-1,1))
         
     
     def dest_coord(self, received_msg):
@@ -42,36 +44,38 @@ class robot_driver():
     
     def pose_coord(self, received_msg):
         self.pose = received_msg
-        self.publish_coord()
+        # self.publish_coord()
         
         
     def publish_coord(self):
         self.direction_finder()
-
-        if abs(self.angle_t_d) > robot_driver.eps:
-            self.vel.linear.x = 0
-            self.vel.angular.z = self.direction
+        dist2go = np.sqrt(self.HT_t_d[0,3]**2 + self.HT_t_d[1,3]**2)
+        
+        if dist2go > robot_driver.dist_arrived:
             
-        else:
-            self.vel.angular.z = 0
-            dist2go = np.sqrt(self.HT_t_d[0,3]**2 + self.HT_t_d[1,3]**2)
-            
-            if dist2go > robot_driver.dist_arrived:
-                self.vel.linear.x = self.drv_spd
-            
-            else:
+            if abs(self.angle_t_d) > robot_driver.eps:
                 self.vel.linear.x = 0
+                self.vel.angular.z = self.direction
+                
+            else:
+                self.vel.angular.z = 0
+                self.vel.linear.x = self.drv_spd
+                
+        else:
+            self.vel.linear.x = 0
+            self.vel.angular.z = 0
             
         self.pub.publish(self.vel)
+        # self.rate.sleep()
                 
         
     def direction_finder(self):
         # self.HT_t_d_calc()
-        R_0_t = self.z_elemental(self.pose.theta)
+        R_t_0 = self.z_elemental(self.pose.theta).T
         q_0 = np.array([self.pose.x, self.pose.y, 0]).reshape(-1,1)
         
-        self.HT_0_t = self.HT_builder(R_0_t, q_0)
-        self.HT_t_d = np.dot(np.linalg.inv(self.HT_0_t), self.HT_0_d)
+        HT_0_t = self.HT_builder(R_t_0, -np.dot(R_t_0, q_0))
+        self.HT_t_d = np.dot(HT_0_t, self.HT_0_d)
         
         self.angle_t_d = np.arctan2(self.HT_t_d[1,3], self.HT_t_d[0,3])
         self.direction = self.rot_spd*np.sign(self.angle_t_d) # if angle is -tiv, must turn CCW (+tiv rot)
@@ -88,6 +92,7 @@ class robot_driver():
     def HT_builder(self, R, p):
         arr_zeros = np.array([[0, 0, 0, 1]])
         return np.vstack((np.hstack((R, p)), arr_zeros))
+        
     
     
     # def HT_t_d_calc(self):        
@@ -104,4 +109,11 @@ if __name__ == '__main__':
     drv_spd = 1
     
     obj = robot_driver(rot_spd, drv_spd)
-    rospy.spin()
+    while not rospy.is_shutdown():
+        try:
+            obj.publish_coord()
+            
+        except rospy.ROSInterruptException:
+            pass
+        obj.rate.sleep()
+    # rospy.spin()
