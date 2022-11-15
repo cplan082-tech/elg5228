509ec0e1b9f551dbc341524809ca416d95499bfc
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-11-15 13:48:43 -0500

    Changed 'pose_pub' from local var to class attribute

--- a2/scripts/pose_reporter.py
+++ a2/scripts/pose_reporter.py
@@ -10,27 +10,34 @@ class pose_reporter():
         
         rospy.init_node('pose_reporter', anonymous=False)
         
-        self.rate = rospy.Rate(2)
+        self.rate = rospy.Rate(2) # 2Hz
         
-        self.sub_pose = rospy.Subscriber('/turtle1/pose', Pose, 
+        # Instantiate subscriber obj for turtle current pose
+        self.sub_pose = rospy.Subscriber('/turtle1/pose', 
+                                         Pose, 
                                          self.callback_pose)
-        self.sub_dest = rospy.Subscriber('/turtle1/destination', Pose, 
+        
+        # Instantiate subscriber obj for desired destination
+        self.sub_dest = rospy.Subscriber('/turtle1/destination', 
+                                         Pose, 
                                          self.callback_dest)
-        self.pub_drv_cmd = rospy.Publisher("turtle1/drv_cmd", Pose,
+        
+        # Instantiate publisher obj
+        self.pub_drv_cmd = rospy.Publisher("turtle1/drv_cmd", 
+                                           Pose,
                                            queue_size=10)
         
-        self.pose = Pose()
-        self.dest = Pose()
+        self.pose = Pose() # Store recived turtle pose
+        self.dest = Pose() # Store recieved destination coords
+        self.pose_pub = Pose() # Store drv cmd's to be published
         
-        # self.HT_0_d = self.HT_builder(self.z_elemental(0), np.array([5.7, 5.7, 0]).reshape(-1,1))
+        # Initialise destination coords/orrientation to turtle starting pose
         self.R_0_d = self.z_elemental(0)
         self.p_0 = np.array([5.5, 5.5, 0]).reshape(-1,1)
         
 
     def callback_pose(self, received_message):
         self.pose = received_message
-        
-        # return rospy.loginfo('Pose = %s', received_message)
     
     
     def callback_dest(self, received_message):
@@ -55,26 +62,25 @@ class pose_reporter():
         dist2go = np.sqrt(HT_t_d[0,3]**2 + HT_t_d[1,3]**2)
         orient_err = np.rad2deg(np.arctan2(HT_t_d[1,3], HT_t_d[0,3]))
         
-        pose_pub = Pose()
-        pose_pub.x = self.pose.x
-        pose_pub.y = self.pose.y
-        pose_pub.theta = np.rad2deg(self.pose.theta)
+        self.pose_pub.x = self.pose.x
+        self.pose_pub.y = self.pose.y
+        self.pose_pub.theta = np.rad2deg(self.pose.theta)
         
-        pose_pub.linear_velocity = dist2go
-        pose_pub.angular_velocity = orient_err
+        self.pose_pub.linear_velocity = dist2go
+        self.pose_pub.angular_velocity = orient_err
         
         rospy.loginfo('\n\n====================Info====================')
-        # rospy.loginfo(pose_pub)
-        self.pub_drv_cmd.publish(pose_pub)
         
         rospy.loginfo('Robot Position:\n\t\t\t\tx= %s m \n\t\t\t\ty= %s m', 
-                      pose_pub.x,
-                      pose_pub.y)
+                      self.pose_pub.x,
+                      self.pose_pub.y)
         rospy.loginfo('Distance between robot and destination:\n\t\t\t\t %s m', 
                       dist2go)
         rospy.loginfo("Robot's current orientation:\n\t\t\t\t %s Deg", 
-                      pose_pub.theta)
+                      self.pose_pub.theta)
         rospy.loginfo('Orientation error:\n\t\t\t\t %s Deg', orient_err)
+        
+        self.pub_drv_cmd.publish(self.pose_pub)
         
         self.rate.sleep()
         
@@ -90,15 +96,7 @@ class pose_reporter():
     def HT_builder(self, R, p):
         arr_zeros = np.array([[0, 0, 0, 1]])
         return np.vstack((np.hstack((R, p)), arr_zeros))
-    
-    
-    # def listener():
-    #     rospy.init_node('pose_reporter', anonymous=False)
-        
-    #     rospy.Subscriber('/turtle1/pose', Pose, callback_pose)
-        
-    #     rospy.spin()
-    
+
 
 if __name__ == '__main__':
     obj = pose_reporter()
