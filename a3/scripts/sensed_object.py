5128af013586222d1af21a1223923b24df984a7f
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-12-03 14:42:24 -0500

    A3: 'sensed_object.py' does not seem to find the indicies of min distance

--- a3/scripts/sensed_object.py
+++ a3/scripts/sensed_object.py
@@ -15,10 +15,19 @@ def callback(msg):
     arr_rng[arr_rng < msg.range_min] = float('NaN')
     
     min_dist = np.nanmin(arr_rng)
-    idx = np.argwhere(arr_rng == min_dist).reshape(-1) # convert 2dim arr to 1 dim arr
-    rospy.loginfo("Min dist. = %7.3f, Angles = %7.3f", 
-                  min_dist, 
-                  np.rad2deg(idx*msg.angle_increment) + msg.angle_min)
+    idx = np.reshape(np.argwhere(arr_rng == min_dist), -1) # convert 2dim arr to 1 dim arr
+    # rospy.loginfo("Min dist. = %7.3f, Angles = %7.3f", 
+    #               min_dist, 
+    #               ((idx*msg.angle_increment)+msg.angle_min)*180.0/np.pi)
+    
+    
+    # rospy.loginfo("Min dist. = %7.3f, Angles = %7.3f", 
+    #               min_dist, 
+    #               idx)
+    
+    rospy.loginfo(idx)
+    
+    # rospy.loginfo("Min dist. = %7.3f", min_dist)
     
     
 def lsr_scn_rd():
