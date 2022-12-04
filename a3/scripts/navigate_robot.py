21e0b124c23be56285077513585466fe30080664
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-12-04 13:34:03 -0500

    A3 part b1: best tunning so far

--- a3/scripts/navigate_robot.py
+++ a3/scripts/navigate_robot.py
@@ -17,11 +17,14 @@ class cls_navigate_robot():
     slow_zone_dist = 1 # error angle in deg where angular vel = min_vel
     
     # proportional gain (K_p) for P type controller
-    kp_ang = 0.8
+    kp_ang = 1
     kp_lin = 0.5
     
-    max_frwd_vel = 0.5
-    max_ang_vel = 0.5
+    max_frwd_vel = kp_lin
+    min_frwd_vel = 0.05
+    
+    max_ang_vel = kp_ang
+    min_ang_vel = 0.1
     
     def __init__(self):
         rospy.init_node('navigate_robot', anonymous=False)
@@ -34,11 +37,6 @@ class cls_navigate_robot():
         self.msg_sensObj = Pose2D()
         self.msg_pose_husky = Twist()
         
-        # For testing vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-        # self.pub_test_sensObj = rospy.Publisher('/test_sensObj', Pose2D, queue_size=10)
-        # self.pub_test_husky_pose = rospy.Publisher('/test_husky_pose', Odometry, queue_size=10)
-        # rospy.spin()
-        # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
         
         
     def callback_sensObj(self, msg):
@@ -48,30 +46,7 @@ class cls_navigate_robot():
         
     def callback_husky_pose(self, msg):
         self.msg_pose_husky = msg
-        # msg_sensObj = self.msg_sensObj
-        
-        # orientation_husky = self.msg_pose_husky.pose.pose.orientation
-        # arr_orientation_husky = np.array([orientation_husky.x,
-        #                                   orientation_husky.y,
-        #                                   orientation_husky.z,
-        #                                   orientation_husky.w])
-        
-        # # numpy arr
-        # arr_orientation_obj = quaternion_from_euler(0, 
-        #                                         0, 
-        #                                         np.deg2rad(self.msg_sensObj.theta))
-        
-        
-        # # arr_quat_obj_husky = quaternion_multiply(arr_orientation_obj, arr_orientation_husky)
-        # arr_quat_obj_husky = quaternion_multiply(arr_orientation_husky, arr_orientation_obj)
-        
-        # print("Robot wrt world: {}".format(np.rad2deg(euler_from_quaternion(arr_orientation_husky)[2])))
-        # print("Obj wrt LiDAR: {}".format(np.rad2deg(euler_from_quaternion(arr_orientation_obj)[2])))
-        # print("Robot wrt LiDAR: {}".format(np.rad2deg(euler_from_quaternion(arr_quat_obj_husky)[2])))
-        # print('\n\n\n\n')
-        
-        # orientation_euler_husky = self.quat2euler(orientation_husky)
-        
+       
         
 
     def publish_cmd_vel(self, x, z):
@@ -84,17 +59,20 @@ class cls_navigate_robot():
         
         
     def find_ang_vel(self, err_angle):       
-        # err_angle = msg_sensObj.theta
-        direction = np.sign(err_angle)*-1  
+        direction = np.sign(err_angle)*-1
+        ang_vel = self.vel_check(abs(cls_navigate_robot.kp_ang*err_angle/180), 
+                                 cls_navigate_robot.min_ang_vel,
+                                 cls_navigate_robot.max_ang_vel)
           
-        return -1*cls_navigate_robot.kp_ang*err_angle/180
+        return direction*ang_vel
     
     
     
-    def find_lin_vel(self, err_dist):
-        # err_dist = msg_sensObj.x      
-        
-        return cls_navigate_robot.kp_lin*err_dist
+    def find_lin_vel(self, err_dist):  
+        lin_vel = self.vel_check(cls_navigate_robot.kp_lin*err_dist,
+                                 cls_navigate_robot.min_frwd_vel,
+                                 cls_navigate_robot.max_frwd_vel)
+        return lin_vel
         
     
     
@@ -115,6 +93,7 @@ class cls_navigate_robot():
         if err_dist < cls_navigate_robot.e:
             lin_vel_x = 0
             ang_vel_z = 0
+            # print('if\n\n\n')
         
         elif abs(err_angle) > cls_navigate_robot.zone_frwd_angle:
             lin_vel_x = 0
@@ -122,17 +101,20 @@ class cls_navigate_robot():
             
         else:
             lin_vel_x = self.find_lin_vel(err_dist)
-            ang_vel_z = self.find_ang_vel(err_angle)
-        
-        # lin_vel_x = self.find_lin_vel(err_dist)
-        
-        # if lin_vel_x > 0:
-        #     ang_vel_z = self.find_ang_vel(err_angle)
             
-        # else:
-        #     ang_vel_z = 0
+            ang_vel_z = self.find_ang_vel(err_angle)
             
         self.publish_cmd_vel(lin_vel_x, ang_vel_z)
+        
+    
+    def vel_check(self, vel, min_vel, max_vel):
+        if vel > max_vel:
+            vel = max_vel
+            
+        elif vel < min_vel:
+            vel = min_vel
+            
+        return vel
         
         
         
