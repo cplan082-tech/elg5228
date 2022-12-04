0ee1e84aa9c368f40e9fff6e269aca2b65871baa
Cplan082 <46877705+Cplan082@users.noreply.github.com>
2022-12-04 15:03:48 -0500

    A3 part b2: Robot approaches within 10 ish cm

--- a3/scripts/navigate_robot.py
+++ a3/scripts/navigate_robot.py
@@ -17,14 +17,14 @@ class cls_navigate_robot():
     slow_zone_dist = 1 # error angle in deg where angular vel = min_vel
     
     # proportional gain (K_p) for P type controller
-    kp_ang = 1
+    kp_ang = 2
     kp_lin = 0.5
     
-    max_frwd_vel = kp_lin
+    max_frwd_vel = kp_ang
     min_frwd_vel = 0.05
     
-    max_ang_vel = kp_ang
-    min_ang_vel = 0.1
+    max_ang_vel = 3
+    min_ang_vel = 0.2
     
     def __init__(self):
         rospy.init_node('navigate_robot', anonymous=False)
@@ -36,6 +36,10 @@ class cls_navigate_robot():
         
         self.msg_sensObj = Pose2D()
         self.msg_pose_husky = Twist()
+        
+        self.track_obj_phase = True
+        self.mv2circ_pos_phase = False
+        self.cir_orientation_set_phase = False
         
         
         
@@ -86,15 +90,19 @@ class cls_navigate_robot():
     
     
     def track_obj(self):
-        msg_sensObj = self.msg_sensObj
-        err_dist = msg_sensObj.x 
-        err_angle = msg_sensObj.theta
+        # msg_sensObj = self.msg_sensObj
+        # err_dist = msg_sensObj.x 
+        # err_angle = msg_sensObj.theta
+        
+        err_dist, err_angle = self.get_obj_err()
         
         if err_dist < cls_navigate_robot.e:
             lin_vel_x = 0
             ang_vel_z = 0
-            # print('if\n\n\n')
-        
+            self.track_obj_phase = False
+            self.mv2circ_pos_phase = True
+            # self.update_target() # TODO: Remove once done testing
+            
         elif abs(err_angle) > cls_navigate_robot.zone_frwd_angle:
             lin_vel_x = 0
             ang_vel_z = self.find_ang_vel(err_angle)
@@ -109,7 +117,50 @@ class cls_navigate_robot():
             
         self.publish_cmd_vel(lin_vel_x, ang_vel_z)
         
+        
+        
+    def update_target(self):
+        msg_pose_husky = self.msg_pose_husky
+        self.husky_trg_position = msg_pose_husky.pose.pose.position
+        self.husky_trg_orientation = msg_pose_husky.pose.pose.orientation
+        
     
+    
+    def dist_from_target(self):
+        msg_pose_husky = self.msg_pose_husky
+        husky_pres_position = msg_pose_husky.pose.pose.position
+        
+        del_x = husky_pres_position.x - self.husky_trg_position.x
+        del_y = husky_pres_position.y - self.husky_trg_position.y
+        
+        dist = np.sqrt(del_x**2 + del_y**2)
+        
+        return dist
+    
+    
+    
+    def move2circ_position(self):
+        err_dist, err_angle = self.get_obj_err()
+        
+        # 0.143 m is dist between lsr and front bumper
+        # 0.337074 m is dist between lsr and outside left wheel
+        if err_dist < cls_navigate_robot.eps_dist + 0.337074: 
+            lin_vel_x = 0
+            self.mv2circ_pos_phase = False
+            self.cir_orientation_set_phase = True
+            
+        else:
+            lin_vel_x = cls_navigate_robot.min_frwd_vel
+        
+        self.publish_cmd_vel(lin_vel_x, 0)
+    
+    
+    def get_obj_err(self):
+        msg_sensObj = self.msg_sensObj
+        
+        return msg_sensObj.x, msg_sensObj.theta
+
+
     def vel_check(self, vel, min_vel, max_vel):
         if vel > max_vel:
             vel = max_vel
@@ -118,6 +169,25 @@ class cls_navigate_robot():
             vel = min_vel
             
         return vel
+    
+    
+    
+    def process_sequencer(self):
+        if self.track_obj_phase:
+            self.track_obj()
+            
+        elif self.mv2circ_pos_phase:
+            self.move2circ_position()
+            
+        elif self.cir_orientation_set_phase:
+            # for testing vvvvvvvvvvvvvvvvvvvvvvvvvvv
+            print('here')
+            self.cir_orientation_set_phase = False
+            self.track_obj_phase = True
+            # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        
+        else:
+            pass
         
         
         
@@ -125,7 +195,7 @@ if __name__ == "__main__":
     obj = cls_navigate_robot()
     while not rospy.is_shutdown():
         try:
-            obj.track_obj()
+            obj.process_sequencer()
         except:
             pass
         
